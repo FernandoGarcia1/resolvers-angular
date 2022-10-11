15b74e90550cd271b50636001cbee334ad7e8dc6
FernandoGarcia1 <fernandogarciadolores@gmail.com>
2022-10-11 18:48:25 -0500

    change in resolver

--- src/app/libs/resolvers/get-character.resolver.ts
+++ src/app/libs/resolvers/get-character.resolver.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import {
   Router, Resolve,
   RouterStateSnapshot,
-  ActivatedRouteSnapshot
+  ActivatedRouteSnapshot,
+  ActivatedRoute,
+  Params
 } from '@angular/router';
 import { catchError, concatMap, Observable, of, tap } from 'rxjs';
 import { RequestService } from 'src/app/services/request.service';
@@ -11,32 +13,56 @@ import { RequestService } from 'src/app/services/request.service';
   providedIn: 'root'
 })
 export class GetCharacterResolver implements Resolve<any> {
+  
 
-  constructor(private characterService: RequestService, private  router: Router) {}
+  constructor(private characterService: RequestService, private  router: Router, public activatedRoute: ActivatedRoute) { }
+  
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
 
+    let exists: boolean = false;
+    let characters : any[] = [];
+    let paramName = route.queryParams['name'];
+    
     //console.log('resolver***** ',route);
 
     /*
     return of({
       message: 'Hola desde Resolver'
     });*/
+
+    
     return this.characterService.getCharacters()
       .pipe(
-        tap((res) =>{
-          console.log('Tap',res);
+        tap((res : any) =>{
+          characters = res.results          
+          console.log(typeof  characters)
+          characters.forEach(character =>{
+            console.log(character['id'],'-',character['name'])
+              if(paramName.toLowerCase() === character['name'].toLowerCase()){
+                console.log(character['name'], ' coincide****')
+                exists = !exists;
+              }
+          }                    
+          )          
+          
         }),
-        concatMap( (res:any) =>{
-          return this.characterService.getCharacter(res.results[6].name);
+        concatMap( (res:any) =>{    
+          if(exists === false){            
+            throw {
+              status: 404
+            };
+          }      
+          console.log('exits',exists)
+          return this.characterService.getCharacter(paramName);
         }
         ),
         catchError((err:any) =>{
           if(err.status === 404){
             this.router.navigate(['404'])
           }
-          console.error(err)
-          return err;
+          console.error('AAA',err)
+          return of(err);
         }
         )
       )
